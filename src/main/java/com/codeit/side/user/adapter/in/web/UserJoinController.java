d1ff0bab7739ce19a090bf9b523dbf939c53073e
coen <coen@mrblue.com>
2025-02-19 09:40:35 +0900

    hotfix: RequestBody annotation 추가

--- src/main/java/com/codeit/side/user/adapter/in/web/UserJoinController.java
+++ src/main/java/com/codeit/side/user/adapter/in/web/UserJoinController.java
@@ -8,10 +8,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,7 +17,7 @@ public class UserJoinController {
     private final UserJoinUseCase userJoinUseCase;
 
     @PostMapping
-    public ResponseEntity<UserJoinResponse> joinUser(@Valid UserJoinRequest request) {
+    public ResponseEntity<UserJoinResponse> joinUser(@RequestBody @Valid UserJoinRequest request) {
         User user = userJoinUseCase.createUser(request.toCommand());
         return ResponseEntity.ok(UserJoinResponse.from(user));
     }
