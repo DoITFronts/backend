8b58377f5304f4cde54ae768bcba52e0c7e4a6fa
coen park <bht9011@gmail.com>
2025-03-13 00:12:46 +0900

    feat: 응답값에서 totalCount 제거

--- src/main/java/com/codeit/side/user/adapter/in/web/response/ReviewInfoResponses.java
+++ src/main/java/com/codeit/side/user/adapter/in/web/response/ReviewInfoResponses.java
@@ -4,11 +4,11 @@ import com.codeit.side.user.domain.ReviewInfos;
 
 import java.util.List;
 
-public record ReviewInfoResponses(List<ReviewInfoResponse> reviews, Integer totalCount) {
+public record ReviewInfoResponses(List<ReviewInfoResponse> reviews) {
     public static ReviewInfoResponses from(ReviewInfos reviewInfos) {
         List<ReviewInfoResponse> reviewInfoResponses = reviewInfos.stream()
                 .map(ReviewInfoResponse::from)
                 .toList();
-        return new ReviewInfoResponses(reviewInfoResponses, reviewInfos.getTotalCount());
+        return new ReviewInfoResponses(reviewInfoResponses);
     }
 }
